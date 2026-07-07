7055023d679151438fa0843223698a562675b83b
Micaela Marshall <micaela@203local.com>
2026-07-07 19:16:14 -0400

    Add shared field schema

---
+++ app/schema/fields.py
@@ -0,0 +1,28 @@
+BUSINESS_ID = "business_id"
+POST_TITLE = "post_title"
+OPERATING_STATUS = "operating_status"
+DIRECTORY_STATUS = "directory_status"
+
+COUNTY = "county"
+TOWN = "town"
+NEIGHBORHOOD = "neighborhood"
+STREET = "street"
+
+PHONE = "phone"
+WEBSITE = "website"
+EMAIL = "email"
+
+INSTAGRAM = "instagram"
+FACEBOOK = "facebook"
+TIKTOK = "tiktok"
+LINKEDIN = "linkedin"
+
+PRIMARY_CATEGORY = "primary_category"
+PRIMARY_BUSINESS_TYPE = "primary_business_type"
+BUSINESS_TYPES = "business_types"
+
+SEARCHABLE_KEYWORDS = "searchable_keywords"
+POST_CONTENT = "post_content"
+
+SEO_DIRECTORY_TITLE = "seo_directory_title"
+SEO_META_DESCRIPTION = "seo_meta_description"
