ca0bf45f8ff6b96378e32e85aa44eb5ca10168b5
Micaela Marshall <micaela@203local.com>
2026-07-08 00:24:38 -0400

    Register website worker

--- app/workers/default_registry.py
+++ app/workers/default_registry.py
@@ -1,7 +1,12 @@
 from app.workers.google_business_worker import GoogleBusinessWorker
+from app.workers.website_worker import WebsiteWorker
 from app.workers.registry import registry
 
 
 def register_default_workers():
     registry.register(GoogleBusinessWorker())
+    registry.register(WebsiteWorker())
     return registry
+
+
+register_default_workers()
